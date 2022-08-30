130fe2cc6bf6d6a3484c70af6cd27508f8d91772
kipraschi <kipraschi@gmail.com>
2022-08-30 11:29:26 +0300

    display error messages and chage border color when passwords don't match

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const password1 = document.querySelector('#password');
-const password2 = document.querySelector('#password-conf');
+const password1 = document.querySelector("#password");
+const password2 = document.querySelector("#password-conf");
+const inputs = document.querySelectorAll("input");
+const passwordError = document.querySelector("#password-conf + div.error-message");
 
-password2.addEventListener("input", validatePasswords);
-password1.addEventListener("input", validatePasswords);
-
+Array.from(inputs).forEach((field) =>
+	field.addEventListener("focusout", validate)
+);
 
 function validatePasswords() {
-    if (password1.value == password2.value) {
-        password1.setCustomValidity("");
-        password2.setCustomValidity("");
-    }
-    else {
-        password1.setCustomValidity("Error");
-        password2.setCustomValidity("Error");
-    }
+	if (password1.value == password2.value && !password1.validity.valueMissing) {
+		password1.setCustomValidity("");
+		password1.className = "filled";
+		password2.setCustomValidity("");
+		password2.className = "filled";
+        passwordError.textContent = "";
+
+	} else {
+        let errorMessage = "* Passwords do not match";
+		password1.setCustomValidity(errorMessage);
+		password1.className = "error";
+		password2.setCustomValidity(errorMessage);
+		password2.className = "error";
+        passwordError.textContent = errorMessage;
+
+	}
+}
+
+function validate() {
+	if (this.type == "password") validatePasswords();
+
+	this.validity.valid
+		? (this.className = "filled")
+		: (this.className = "error");
 }
